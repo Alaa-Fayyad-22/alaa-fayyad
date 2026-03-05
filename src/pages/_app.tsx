580dddb29033efe3dfc0a2ecd39578433a3cde29
AlaaFayyad <cr905143@gmail.com>
2026-03-05 10:14:42 +0200

    Remove next-themes from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider
-      attribute="class"       // adds/removes class="dark" on <html>
-      defaultTheme="dark"
-      enableSystem={false}
-      disableTransitionOnChange={false}
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
